a5adffc1a4dea089bc17aab91993fbb2f23d0fe0
ASUS <ASUS@DESKTOP-V2544C7>
2021-12-20 01:24:43 +0300

    Day38_Overriding

---
+++ Happy/src/Day38_Overriding/Kuslar.java
@@ -0,0 +1,19 @@
+package Day38_Overriding;
+
+public class Kuslar extends Animals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void hareket() {
+		System.out.println("kuslar ucarak hareket eder");
+		
+	}
+	
+	public void solunum() {
+		System.out.println("kuslar akcigerleriyle nefes alir");
+		
+	}
+}
