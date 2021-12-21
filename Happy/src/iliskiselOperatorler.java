9fdc29ace947c5f2f106e66e5d98679c4ef49229
ASUS <ASUS@DESKTOP-V2544C7>
2021-12-21 19:54:07 +0300

    iliskiselOperatorler

---
+++ Happy/src/iliskiselOperatorler.java
@@ -0,0 +1,31 @@
+
+public class iliskiselOperatorler {
+
+	public static void main(String[] args) {
+		/*
+		 * Operatör	Açýklama
+			==		Eþit mi?
+			!=		Eþit-deðil mi?
+			>		Büyük mü?
+			<		Küçük mü?
+			>=		Büyük veya Eþit mi?
+			<=		Küçük veya Eþit mi?
+		 */
+		int sayi1=100;
+		int sayi2=90;
+		
+		//sayi1> sayi2= boolean primitive data type (true-false dondurur.)
+		System.out.println(sayi1 == sayi2); // Eþit mi? : false
+		
+		System.out.println(sayi1 != sayi2); // Eþit-deðil mi? : true
+		
+		System.out.println(sayi1 > sayi2); // Büyük mü?  : true
+		
+		System.out.println(sayi1 < sayi2); // Küçük mü? : false
+		
+		System.out.println(sayi1 >= sayi2); // Büyük veya Eþit mi? : true
+		
+		System.out.println(sayi1 <= sayi2); // Küçük veya Eþit mi? : false
+	}
+
+}
