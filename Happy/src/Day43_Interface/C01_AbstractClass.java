a8ca8b8933bf92cdc22b84f35c2077b616580786
ASUS <ASUS@DESKTOP-V2544C7>
2021-12-22 00:26:29 +0300

    Day43_interface

---
+++ Happy/src/Day43_Interface/C01_AbstractClass.java
@@ -0,0 +1,30 @@
+package Day43_Interface;
+
+public class C01_AbstractClass {
+
+	C01_AbstractClass(){
+	
+	}
+	
+	public static void main(String[] args) {
+		// abstract class'lar CLASS olduklari icin constructor'a sahiptirler
+		// ancak abstract class'lardan obje OLUSTURULAMAZ
+		
+		
+			
+		}
+		
+	
+			
+			
+			
+			// C01_AbstractClass obj1 =new C01_AbstractClass();
+
+	
+
+	
+		// TODO Auto-generated method stub
+		
+	}
+
+
