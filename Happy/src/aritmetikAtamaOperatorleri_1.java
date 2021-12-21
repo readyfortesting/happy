c786074583890ebc822060e049d292b4dd5d88c3
ASUS <ASUS@DESKTOP-V2544C7>
2021-12-21 17:40:41 +0300

    Aritmetik Atama Operatoru 25 ve 5 sayilarini kullanarak 4 islem yapiniz

---
+++ Happy/src/aritmetikAtamaOperatorleri_1.java
@@ -0,0 +1,39 @@
+
+public class aritmetikAtamaOperatorleri_1 {
+
+	public static void main(String[] args) {
+		
+		// 25 ve 5 sayilarini kullanarak 4 islem yapiniz, 4 islemi uygularken aritmetik atama operatorlerini kullaniniz
+		
+		
+		/* 25 + 5
+		 * 25 - 5
+		 * 25 * 5
+		 * 25 / 5 
+		 * 
+		 * 25 += 5
+		 * 25 -= 5
+		 * 25 *= 5
+		 * 25 /= 5
+		 * 
+		 */
+		
+		
+			int sayi1=25;
+			int sayi2=5;
+			
+			sayi1 += sayi2;
+			System.out.println(sayi1); // sayi1 = 30
+			
+			sayi1 -= sayi2;
+			System.out.println(sayi1);  // sayi1 = 25
+			
+			sayi1 *= sayi2;
+			System.out.println(sayi1); // sayi1 = 125
+			
+			sayi1 /= sayi2;
+			System.out.println(sayi1); // sayi1 = 25
+			
+	}
+
+}
